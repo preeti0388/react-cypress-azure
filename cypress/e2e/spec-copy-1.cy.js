3598333d39ff32fe21b85b35c4e0b7a2b5414949
preeti <preeti0388@gmail.com>
2024-02-16 16:01:10 +1100

    bootstrap

---
+++ cypress/e2e/spec-copy-1.cy.js
@@ -0,0 +1,41 @@
+describe('JSONPlaceholder API Tests', () => {
+  it('should fetch and verify user data', () => {
+    cy.request('GET', 'https://jsonplaceholder.typicode.com/users/1')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.id).to.eq(1);
+        expect(response.body.name).to.eq('Leanne Graham');
+      });
+  });
+
+  it('should create a new post', () => {
+    const postData = {
+      title: 'Test Post',
+      body: 'This is a test post',
+      userId: 1
+    };
+
+    cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', postData)
+      .then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body.title).to.eq('Test Post');
+        expect(response.body.body).to.eq('This is a test post');
+        expect(response.body.userId).to.eq(1);
+      });
+  });
+
+  it('should fetch and verify todos', () => {
+    cy.request('GET', 'https://jsonplaceholder.typicode.com/todos')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.length).to.be.greaterThan(0);
+      });
+  });
+  it('hell preeti', () => {
+    cy.request('GET', 'https://jsonplaceholder.typicode.com/todos')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.length).to.be.greaterThan(0);
+      });
+  });
+});
